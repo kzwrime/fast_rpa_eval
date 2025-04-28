b322d19f30d0c48ac2e85e66942228f38ddb529d
kunzh <zhikun.wu@outlook.com>
2025-04-28 20:58:23 +0800

    [fix] Fix Makefile

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@
 source /opt/intel/oneapi/mkl/latest/env/vars.sh
 
 # Add magma lib path
-export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$(pwd)/third_party/local/lib:$LD_LIBRARY_PATH
